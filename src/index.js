2773fb29f202df3f9f75297a92316af81aec30c8
othman19t <othman19t@gmail.com>
2020-05-14 21:44:52 -0700

    commanting

--- src/index.js
+++ src/index.js
@@ -6,30 +6,34 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// save data into users collection when post request is made
 app.post("/users", (req, res) => {
   const user = new User(req.body);
   user
     .save()
     .then((result) => {
-      res.send(result);
+      res.status(201).send(result);
     })
     .catch((err) => {
       res.status(400).send(err);
     });
 });
 
+// save data into tasks collection when post request is made
 app.use("/task", (req, res) => {
   const task = new Task(req.body);
   task
     .save()
     .then((result) => {
-      res.send(result);
+      res.status(201).send(result);
     })
     .catch((err) => {
       res.status(400).send(err);
     });
 });
 
+// listening on post 3000 any other
 app.listen(port, () => {
   console.log(`server is up on port ${port}`);
 });
--- src/models/task.js
+++ src/models/task.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Task = mongoose.model("Task", {
   description: {
     type: String,
+    required: true,
   },
   completed: {
     type: Boolean,
