915a58de0c6bce1114149f3b39af1681df2a5c81
othman19t <othman19t@gmail.com>
2020-05-16 19:29:55 -0700

    #94 load all tasks

--- src/models/playGround.js
+++ src/models/playGround.js
@@ -1,5 +1,6 @@
 require("../db/mongoose");
 const User = require("../models/user");
+const Task = require("../models/task");
 
 // update record and count records
 const id = "5ebdfef9bef8661ebd47cf2e";
@@ -9,6 +10,16 @@ User.findByIdAndUpdate(id, { age: 1000 })
     return User.countDocuments({ age: 1000 }).then((result) => {
       console.log(result);
     });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+//load all tasks
+Task.find({})
+  .then((result) => {
+    console.log(result);
   })
   .catch((err) => {
     console.log(err);
